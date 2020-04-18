fc462d59848d553d3d18883179c22f38f9834d74
mecraze <alok.edasseri@gmail.com>
2020-04-18 18:15:06 +0530

    Adding Php
    Added 'reuired' in input fields
    Added a text php file

---
+++ udemy/process.php
@@ -0,0 +1,24 @@
+<?php
+
+if(isset($_POST['submit'])){
+    
+echo $_POST ['subject'];
+        
+        
+}
+
+
+
+?>
+
+
+<!--
+$to="alok.edasseri@gmail.com";
+$subject=$_POST['subject'];
+$email=$_POST['email'];
+$txt=$_POST['message'];
+$headers="From:".$email."\r\n".
+"CC: somebodyelse@example.com";
+        
+mail($to,$subject,$txt,$headers);
+header("Location: contact.html");-->
